b4e471400eeaf5b22d87110cf1cf731206b6687c
Michael Wornow <miking98@fb.com>
2017-07-03 15:04:28 -0700

    Basic layout for home feed and 20 tweets fill table view

--- twitter/TweetCell.swift
+++ twitter/TweetCell.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class TweetCell: UITableViewCell {
     
-    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var screenNameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    @IBOutlet weak var replyLabel: UILabel!
+    @IBOutlet weak var retweetLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
     
     var tweet: Tweet! {
         didSet {
-            tweetTextLabel.text = tweet.text
+            nameLabel.text = tweet.user.name
+            screenNameLabel.text = tweet.user.screenName
+            dateLabel.text = tweet.createdAtString
+            tweetTextView.text = tweet.text
+            
+            retweetLabel.text = String(format: "%d", tweet.retweetCount)
+            favoriteLabel.text = String(format: "%d", tweet.favoriteCount)
         }
     }
     
