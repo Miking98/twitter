04ce7647d717339c0fe3c228c454171053166aa7
Michael Wornow <miking98@fb.com>
2017-07-03 14:05:43 -0700

    Logout functionality

--- twitter/APIManager.swift
+++ twitter/APIManager.swift
@@ -52,8 +52,6 @@ class APIManager: SessionManager {
     
     func logout() {
         clearCredentials()
-        
-        // TODO: Clear current user by setting it to nil
 
         NotificationCenter.default.post(name: NSNotification.Name("didLogout"), object: nil)
     }
